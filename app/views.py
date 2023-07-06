5efa5aae0e3f6ddc8fd58e83f66104c93ae3affc
praveensagar22 <praveensagarbingi@gmail.com>
2023-07-06 19:36:21 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import *
+from .models import *
+
+
+@api_view(['GET'])
+def home(request):
+    question_obj = Question.objects.all()
+    serializer = QuizSerializer(question_obj, many=True)
+    return Response({'status': 200, "message": 'hello', 'payload': serializer.data})
